363374c412913d63490e4754770a35e6622137a9
Eduardo Amorim <63356025+Edu-Amorim@users.noreply.github.com>
2025-06-08 08:34:15 -0300

    Added Portuguese translations

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,6 +2,8 @@ import { changeLanguage, use } from "i18next"
 import { initReactI18next } from "react-i18next"
 import en from "./locales/en.json"
 import fr from "./locales/fr.json"
+import pt from "./locales/pt.json"
+
 
 export const resources = {
   en: {
@@ -11,6 +13,10 @@ export const resources = {
   fr: {
     label: "Français",
     translation: fr,
+  },
+  pt: {
+    label: "Português",
+    translation: pt,
   },
 }
 
