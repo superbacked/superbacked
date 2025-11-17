4085ed91658d919fe5e495f555f1e9a08bfa54c4
Sun Knudsen <hello@sunknudsen.com>
2025-11-16 19:23:46 -0500

    Deleted extraneous utility

--- src/main/utilities/sleep.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Sleep for delay
- * @param delay delay in milliseconds
- * @returns promise
- */
-export default async (delay: number) => {
-  return new Promise((resolve) => setTimeout(resolve, delay))
-}
