0771ab2740d74567a20160db6ef1a8340ae3a22f
Sun Knudsen <hello@sunknudsen.com>
2026-07-04 12:29:38 -0400

    Fixed webpack consumers issue

--- webpack.plugins.ts
+++ webpack.plugins.ts
@@ -1,5 +1,10 @@
+import { config } from "dotenv"
 import Dotenv from "dotenv-webpack"
 
-export const createPlugins = (env: "development" | "production") => [
-  new Dotenv({ path: `./.env.${env}` }),
-]
+export const createPlugins = (env: "development" | "production") => {
+  const path = `./.env.${env}`
+  // Provide environment variables to webpack consumers
+  config({ path, quiet: true })
+  // Bundle environment variables
+  return [new Dotenv({ path })]
+}
